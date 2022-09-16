f4a7aecd5160a2c6cdc4f1bf9f0fbcdbcfff8fd5
sam <abluecar@gmail.com>
2022-09-15 23:03:11 -0400

    Populate pawns on board

--- lib/chess.rb
+++ lib/chess.rb
@@ -8,12 +8,25 @@ class Chess
     @board = board || Board.new
     @player1 = player1 || Player.new(name: 'Player One')
     @player2 = player2 || Player.new(name: 'Player Two')
-    create_chess_pieces
+    white_set = PieceFactory.create_set('white')
+    black_set = PieceFactory.create_set('black')
+    pieces = { white_pcs: white_set, black_pcs: black_set }
+    set_up_board(pieces)
+  end
+
+  def set_up_board(chess_pieces)
+    [1, 6].each do |i|
+      (0..7).each do |x|
+        board.squares[i][x] = chess_pieces[0][x] if i == 1
+        board.squares[i][x] = chess_pieces[1][x] if i == 6
+      end
+    end
   end
 
   def create_chess_pieces
-    @board.squares[1][0] = PieceFactory.create('Pawn', 'white')
-    @board.squares[6][0] = PieceFactory.create('Pawn', 'black')
+
+    # @board.squares[1][0] = PieceFactory.create('Pawn', 'white')
+    # @board.squares[6][0] = PieceFactory.create('Pawn', 'black')
   end
 
   def play
--- lib/piece_factory.rb
+++ lib/piece_factory.rb
@@ -1,22 +1,14 @@
 # This class creates game piece objects
 class PieceFactory
+  def self.create_set(color)
+    set = Array.new(8, 'Pawn')
+    set.map { |item| self.create(item, color) }
+    # set << self.create('Pawn', color)
+    # set
+  end
+
   def self.create(piece, color)
     Object.const_get(piece).new(color)
-
-    # Object.const_get('Array').new
-    # case piece
-    # when 'king'
-    # when 'queen'
-    #   Queen.new(color)
-    # when 'rook'
-    #   Rook.new(color)
-    # when 'knight'
-    #   Knight.new(color)
-    # when 'bishop'
-    #   Bishop.new(color)
-    # when 'pawn'
-    #   Pawn.new(color)
-    # end
   end
 end
 
