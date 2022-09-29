1d3332c5dee707b6fdb37bb1d90514b92e8d8106
sam <abluecar@gmail.com>
2022-09-28 23:44:17 -0400

    Finish reworking Castle#revert_board

--- lib/special_moves/castle.rb
+++ lib/special_moves/castle.rb
@@ -32,7 +32,7 @@ class Castle < Move
   end
 
   def king_side_castle?
-    corner_piece = board.object([start_sq[0] + 0, start_sq[1] + 3])
+    corner_piece = board.object([start_sq[0], start_sq[1] + 3])
     return false unless corner_piece.instance_of?(Rook)
     return false unless board.object([start_sq[0], start_sq[1] + 1]) == 'unoccupied'
     return false unless board.object([start_sq[0], start_sq[1] + 2]) == 'unoccupied'
@@ -40,7 +40,7 @@ class Castle < Move
   end
 
   def queen_side_castle?
-    corner_piece = board.object([start_sq[0] + 0, start_sq[1] - 4])
+    corner_piece = board.object([start_sq[0], start_sq[1] - 4])
     return false unless corner_piece.instance_of?(Rook)
     return false unless board.object([start_sq[0], start_sq[1] - 1]) == 'unoccupied'
     return false unless board.object([start_sq[0], start_sq[1] - 2]) == 'unoccupied'
@@ -63,30 +63,61 @@ class Castle < Move
     board.update_square(start_sq, 'unoccupied')
 
     if base_move == [0, 2]
-      corner = [start_sq[0] + 0, start_sq[1] + 3]
-      new_sq = [start_sq[0] + 0, start_sq[1] + 1]
+      corner = [start_sq[0], start_sq[1] + 3]
+      rook_new_sq = [start_sq[0], start_sq[1] + 1]
       rook = board.object(corner)
     elsif base_move == [0, -2]
-      corner = [start_sq[0] + 0, start_sq[1] - 4]
-      new_sq = [start_sq[0] + 0, start_sq[1] - 1]
+      corner = [start_sq[0], start_sq[1] - 4]
+      rook_new_sq = [start_sq[0], start_sq[1] - 1]
       rook = board.object(corner)
     end
   
-    board.update_square(new_sq, rook) # rook
+    board.update_square(rook_new_sq, rook) # rook
     board.update_square(corner, 'unoccupied')
   end
+
+  # def revert_board
+  #   puts "\n\t#{self.class}##{__method__}\n "
+  #   board.update_square(end_sq, end_obj)
+  #   board.update_square(start_sq, start_piece)
+  # end
 
   # override
   def revert_board
     puts "\n\t#{self.class}##{__method__}\n "
-    board.update_square(new_sq, rook) # rook
-    board.update_square(new_sq, rook) # rook
-    board.update_square(new_sq, rook) # rook
-    board.update_square(new_sq, rook) # rook
+    base_move = base_move(start_sq, end_sq, board.object(end_sq).color)
+    temp = start_piece.invert(base_move) if player.color == 'black'
+    base_move = temp if player.color == 'black'
+
+    if base_move == [0, 2]
+      corner = [start_sq[0], start_sq[1] + 3]
+      rook_sq = [start_sq[0], start_sq[1] + 1]
+      rook = board.object([start_sq[0], start_sq[1] + 1])
+      # rook = board.object(corner)
+    elsif base_move == [0, -2]
+      corner = [start_sq[0], start_sq[1] - 4]
+      rook_sq = [start_sq[0], start_sq[1] - 1]
+      rook = board.object([start_sq[0], start_sq[1] - 1])
+      # rook = board.object(corner)
+    end
+
+    king = board.object(end_sq)
+
+    # - revert board, revert everything we moved:
+    # -- king goes back to home spot
+    # -- rook goes back in the corner
+    # -- the 2 middle spaces become unoccupied
+
+    board.update_square(corner, rook) # move rook back to corner
+    board.update_square(start_sq, king) # move king back to home spot
+    board.update_square(rook_sq, 'unoccupied') # middle space back to 'unoccupied'
+    board.update_square(end_sq, 'unoccupied') # middle space back to 'unoccupied'
+    
+    # Display.draw_board(board)
   end
 
   def validate_move
-    raise NotImplementedError, 'Update King and Rook to moved'
+    raise NotImplementedError, 'Update King and Rook to unmoved'
     # @validated = true
     # start_piece.moved
   end
