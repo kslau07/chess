72648f176d5f337c3ded86aac5a556b6070d82e3
sam <abluecar@gmail.com>
2022-12-11 21:33:38 -0500

    test: add piece and pawn files

---
+++ spec/pawn_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+describe Pawn do
+  describe '#to_s' do
+  
+  end
+
+  describe '#moved' do
+  
+  end
+
+  describe '#make_path' do
+  
+  end
+
+  describe '#make_single_step_path' do
+  
+  end
+
+  describe '#make_double_step_path' do
+  
+  end
+
+  describe '#make_capture_path' do
+  
+  end
+
+  describe '#capturable_squares' do
+  
+  end
+end
---
+++ spec/piece_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+describe Piece do
+  describe '#invert' do
+  
+  end
+
+  describe '#make_path' do
+  
+  end
+
+  describe '#make_capture_path' do
+  
+  end
+end
