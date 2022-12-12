975d23bd930d3dbeae59d23b9d2fc376d6bb1102
sam <abluecar@gmail.com>
2022-12-12 12:41:42 -0500

    test: Bishop#make_path

--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -6,6 +6,18 @@ require_relative '../../lib/pieces/bishop'
 describe Bishop do
   let(:wht_bishop) { described_class.new(color: 'white') }
   let(:blk_bishop) { described_class.new(color: 'black') }
+  let(:board) { instance_double('board', squares: board_squares) }
+
+  # method from class Board for Board double
+  def board_squares
+    arr_of_squares = []
+    8.times do |x|
+      8.times do |y|
+        arr_of_squares << [x, y]
+      end
+    end
+    arr_of_squares
+  end
 
   describe '#to_s' do
     context 'when Bishop is white' do
@@ -39,5 +51,46 @@ describe Bishop do
     end
   end
 
-  
+  # superclass method
+  describe '#make_path' do
+    context 'for white Bishop' do
+      context 'when start_sq is [2, 3]' do
+        start_sq = [2, 3]
+
+        context 'when Bishop moves diagonally' do
+          it 'returns a path (Array) that is not empty' do
+            end_sq = [6, 7]
+
+            result = wht_bishop.make_path(board, start_sq, end_sq)
+            expect(result).to eq [[2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]
+          end
+
+          it 'returns a path (Array) that is not empty' do
+            end_sq = [0, 1]
+
+            result = wht_bishop.make_path(board, start_sq, end_sq)
+            expect(result).to eq [[2, 3], [1, 2], [0, 1]]
+          end
+        end
+
+        context 'when Bishop tries to move sideways' do
+          it 'returns an empty Array' do
+            end_sq = [2, 7]
+
+            result = wht_bishop.make_path(board, start_sq, end_sq)
+            expect(result).to be_empty
+          end
+        end
+
+        context 'when Bishop tries to move vertically' do
+          it 'returns an empty Array' do
+            end_sq = [6, 3]
+
+            result = wht_bishop.make_path(board, start_sq, end_sq)
+            expect(result).to be_empty
+          end
+        end
+      end
+    end
+  end
 end
